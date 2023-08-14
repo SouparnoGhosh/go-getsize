4e21d66ccb5e640725d9f532d0ec1163da48d531
SouparnoGhosh <ghosh99souparno@gmail.com>
2023-08-14 12:38:57 +0530

    First Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "fmt"
+
+	"github.com/souparno/go-getsize/cmd"
+)
+
+func main() {
+	cmd.Execute()
+	// err := cmd.Execute(); if err {
+	// 	if err != nil {
+	// 		fmt.Printf("Error occured: %v", err)
+	// 	}
+	// }
+}
